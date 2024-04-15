95b8c5b5db87cd8d39d8b14a06d0808a4064f138
AsadFahimTEC <asaduzzamanfahim838@gmail.com>
2024-04-15 19:12:16 +0600

    design finish

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+const autoprefixer = require('autoprefixer');
+const tailwindcss = require('tailwindcss');
+
+module.exports = {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ],
+};
